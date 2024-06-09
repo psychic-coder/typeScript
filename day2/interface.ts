3328b97ce349350372aae3088a8493ee38394748
psychic-coder <rohitganguly450@gmail.com>
2024-06-09 09:03:04 +0530

    intrerface

--- day2/interface.ts
+++ day2/interface.ts
@@ -13,18 +13,23 @@ interface User{
 
     getCoupon(coupanname:string):number
 }
-const hitesh:User={
-    dbId:1312,
-    email:"dedw",
-    userId:13,
-
-    //we defined a method in the object
-    startTrail:()=>{
-        return "tri"
-    },
-
-    getCoupon:(name:"roh10")=>{
-        return 20
-    }
+// we have added the githubToken in the above interface
+interface User{
+    githubToken?:string
 }
 
+//now the interface contains the github token
+const rohit:User={
+    dbId:3431,
+    email:"13",
+    userId:313,
+    startTrail:()=>"Fwf",
+    getCoupon:(coupanname:"w3r")=>31,
+    githubToken:"fwefwef"
+}
+
+//we can even do the inheritance in interface
+//adminnwill have all the properties of user alongwith its own properties
+interface admin extends User{
+role:"we"|"Wfw"|"fw"
+}
